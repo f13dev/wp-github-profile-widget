79afc5884e5edfd722cc99cde744b904588af103
James Valentine <jv@f13dev.com>
2016-07-12 17:10:53 +0100

    Starting to integrate API

--- github-profile-widget.php
+++ github-profile-widget.php
@@ -154,58 +154,87 @@ class GitHub_Mini_Profile_Widget extends WP_Widget
 	{
 		extract($instance);
 
-		/**
-		 * Require the twitter.php file to load the widget content
-		 */
+		// Get the API results
+		$results = $this->f13_get_github_api($github_user);
 		$widget = '
 			<div class="gmpw-container">
-				<div class="gmpw-head">
-				  <div class="gmpw-headder">
-						GitHub @f13dev
-					</div>
-					<div class="gmpw-profile-picture">
-						<img src="https://avatars1.githubusercontent.com/u/18120373?v=3&s=460"  />
-					</div>
-					<div class="gmpw-names">
-						<div class="gmpw-name">
-							James Valentine
+				<a href="https://github.com/' . $results['login'] . '" class="gmpw-head-link">
+					<div class="gmpw-head">
+					  <div class="gmpw-headder">
+							<img src="' . $results['avatar_url'] . '" />
+							GitHub
 						</div>
-						<div class="gmpw-user">
-							@f13dev
+						<div class="gmpw-profile-picture">
+							<img src="https://avatars1.githubusercontent.com/u/18120373?v=3&s=460"  />
 						</div>
-					</div>
-
+						<div class="gmpw-names">
+							<div class="gmpw-name">
+								' . $results['name'] . '
+							</div>
+							<div class="gmpw-user">
+								@' . $results['login'] . '
+							</div>
+						</div>
+					</a>
 				</div>
 				<div class="gmpw-bio">
-					BSc (Hons) Computing & IT (Software Development) student with the Open University. A messy PHP coder of 10+ years. Scribbling bits of code on GitHub.
+					<span>Bio: </span>
+					' . $results['bio'] . '
 				</div>
 				<div class="gmpw-info">
 					<span class="gmpw-info-user">
 						<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 14 16" width="14"><path d="M4.75 4.95C5.3 5.59 6.09 6 7 6c.91 0 1.7-.41 2.25-1.05A1.993 1.993 0 0 0 13 4c0-1.11-.89-2-2-2-.41 0-.77.13-1.08.33A3.01 3.01 0 0 0 7 0C5.58 0 4.39 1 4.08 2.33 3.77 2.13 3.41 2 3 2c-1.11 0-2 .89-2 2a1.993 1.993 0 0 0 3.75.95zm5.2-1.52c.2-.38.59-.64 1.05-.64.66 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2-.65 0-1.17-.53-1.19-1.17.06-.19.11-.39.14-.59zM7 .98c1.11 0 2.02.91 2.02 2.02 0 1.11-.91 2.02-2.02 2.02-1.11 0-2.02-.91-2.02-2.02C4.98 1.89 5.89.98 7 .98zM3 5.2c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.45 0 .84.27 1.05.64.03.2.08.41.14.59C4.17 4.67 3.66 5.2 3 5.2zM13 6H1c-.55 0-1 .45-1 1v3c0 .55.45 1 1 1v2c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-1h1v3c0 .55.45 1 1 1h2c.55 0 1-.45 1-1v-3h1v1c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-2c.55 0 1-.45 1-1V7c0-.55-.45-1-1-1zM3 13H2v-3H1V7h2v6zm7-2H9V9H8v6H6V9H5v2H4V7h6v4zm3-1h-1v3h-1V7h2v3z"></path></svg>
-						F13 Dev<br />
-						<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 12 16" width="12"><path d="M6 0C2.69 0 0 2.5 0 5.5 0 10.02 6 16 6 16s6-5.98 6-10.5C12 2.5 9.31 0 6 0zm0 14.55C4.14 12.52 1 8.44 1 5.5 1 3.02 3.25 1 6 1c1.34 0 2.61.48 3.56 1.36.92.86 1.44 1.97 1.44 3.14 0 2.94-3.14 7.02-5 9.05zM8 5.5c0 1.11-.89 2-2 2-1.11 0-2-.89-2-2 0-1.11.89-2 2-2 1.11 0 2 .89 2 2z"></path></svg>
-						Margate, United Kingdom
+						' . $results['login'] . '<br />';
+
+						if ($results['location'] != '')
+						{
+							$widget .= '
+							<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 12 16" width="12"><path d="M6 0C2.69 0 0 2.5 0 5.5 0 10.02 6 16 6 16s6-5.98 6-10.5C12 2.5 9.31 0 6 0zm0 14.55C4.14 12.52 1 8.44 1 5.5 1 3.02 3.25 1 6 1c1.34 0 2.61.48 3.56 1.36.92.86 1.44 1.97 1.44 3.14 0 2.94-3.14 7.02-5 9.05zM8 5.5c0 1.11-.89 2-2 2-1.11 0-2-.89-2-2 0-1.11.89-2 2-2 1.11 0 2 .89 2 2z"></path></svg>
+							' . $results['location'] . '<br />';
+						}
+
+						if ($results['email'] != '')
+						{
+							$widget .= '
+							<svg aria-hidden="true" class="octicon octicon-mail" height="16" version="1.1" viewBox="0 0 14 16" width="14"><path d="M0 4v8c0 .55.45 1 1 1h12c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1H1c-.55 0-1 .45-1 1zm13 0L7 9 1 4h12zM1 5.5l4 3-4 3v-6zM2 12l3.5-3L7 10.5 8.5 9l3.5 3H2zm11-.5l-4-3 4-3v6z"></path></svg>
+							<a href="mailto:' . $results['email'] . '">' . $results['email'] . '</a>';
+						}
+
+					$widget .= '
 					</span>
-					<span class="gmpw-info-website">
-						<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>
-						<a href="#">http://f13dev.com</a><br />
-						<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>
+					<span class="gmpw-info-website">';
+
+						if ($results['blog'] != '')
+						{
+							$widget .= '
+							<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>
+							<a href="' . $results['blog'] . '">' . $results['blog'] . '</a><br />';
+						}
+
+						$widget .= '
+						<svg aria-hidden="true" class="octicon octicon-clock" height="16" version="1.1" viewBox="0 0 14 16" width="14"><path d="M8 8h3v2H7c-.55 0-1-.45-1-1V4h2v4zM7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7z"></path></svg>
 						Joined on Mar 28, 2016
 					</span>
 				</div>
 				<div class="gmpw-numbers">
-					<span>
-						1<br />
-						Follower
-					</span>
-					<span>
-						0<br />
-						Starred
-					</span>
-					<span>
-						9<br />
-						Following
-					</span>
+					<a href="#">
+						<span>
+							<span>' . $results['followers'] . '</span><br />
+							Follower
+						</span>
+					</a>
+					<a href="#">
+						<span>
+							<span>0</span><br />
+							Starred
+						</span>
+					</a>
+					<a href="#">
+						<span>
+							<span>' . $results['following'] . '</span><br />
+							Following
+						</span>
+					</a>
 				</div>
 				<div class="gmpw-repos">
 					<span class="gmpw-repos-public">15 Public Repositories</span>
@@ -215,4 +244,52 @@ class GitHub_Mini_Profile_Widget extends WP_Widget
 		';
 		echo $widget;
 	}
+
+	private function f13_get_github_api($github_user)
+	 {
+			 // Start curl
+			 $curl = curl_init();
+			 // Set the url
+			 $url = 'https://api.github.com/users/' . $github_user;
+			 // Set curl options
+			 curl_setopt($curl, CURLOPT_URL, $url);
+			 curl_setopt($curl, CURLOPT_HTTPGET, true);
+
+			 // Check if a token is set
+			 if (preg_replace('/\s+/', '', $this->github_token) != '' || $this->github_token != null)
+			 {
+					 // If a token is set attempt to send it in the header
+					 curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+							 'Content-Type: application/json',
+							 'Accept: application/json',
+							 'Authorization: token ' . $this->github_token
+					 ));
+			 }
+			 else
+			 {
+					 // If no token is set, send the header as unauthenticated,
+					 // some features may not work and a lower rate limit applies.
+					 curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+							 'Content-Type: application/json',
+							 'Accept: application/json'
+					 ));
+			 }
+			 // Set the user agent
+			 curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+			 // Set curl to return the response, rather than print it
+			 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+			 // Get the results
+			 $result = curl_exec($curl);
+
+			 // Close the curl session
+			 curl_close($curl);
+
+			 // Decode the results
+			 $result = json_decode($result, true);
+
+			 // Return the results
+			 return $result;
+	 }
+
 }
