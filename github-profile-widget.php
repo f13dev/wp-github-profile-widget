a56868e748c5275d83021517c8a8f2d6d6ba28a3
James Valentine <jv@f13dev.com>
2016-07-12 20:10:47 +0100

    Added caching capability

--- github-profile-widget.php
+++ github-profile-widget.php
@@ -62,10 +62,10 @@ class GitHub_Mini_Profile_Widget extends WP_Widget
 		$this->textdomain = strtolower(get_class($this));
 
 		//Add fields
-		$this->add_field('title', 'Enter title', '', 'text');
+		$this->add_field('title', 'Widget title', '', 'text');
 		$this->add_field('github_user', 'GitHub ID', '', 'text');
 		$this->add_field('github_token', 'GitHub API Token', '', 'text');
-		$this->add_field('option_show_figures', 'Show figures', true, 'checkbox');
+		$this->add_field('github_timeout', 'Cache timeout (minutes)', '30', 'number');
 		//Init the widget
 		parent::__construct($this->textdomain, __(self::WIDGET_NAME, $this->textdomain), array( 'description' => __(self::WIDGET_DESCRIPTION, $this->textdomain), 'classname' => $this->textdomain));
 	}
@@ -138,6 +138,16 @@ class GitHub_Mini_Profile_Widget extends WP_Widget
 
 			}
 			else
+			if($field_data['type'] === 'number')
+			{
+				?>
+				<p>
+					<label for="<?php echo $this->get_field_id($field_name); ?>"><?php _e($field_data['description'], $this->textdomain ); ?></label>
+					<input class="widefat" id="<?php echo $this->get_field_id($field_name); ?>" name="<?php echo $this->get_field_name($field_name); ?>" type="number" value="<?php echo esc_attr(isset($instance[$field_name]) ? $instance[$field_name] : $field_data['default_value']); ?>" />
+				</p>
+				<?php
+			}
+			else
 			{
 				/* Otherwise show an error */
 				echo __('Error - Field type not supported', $this->textdomain) . ': ' . $field_data['type'];
@@ -164,115 +174,127 @@ class GitHub_Mini_Profile_Widget extends WP_Widget
 	{
 		extract($instance);
 
-		// Get the API results
-		$userAPI = $this->f13_get_github_api('https://api.github.com/users/' . $github_user);
-		$widget = '
-			<div class="gmpw-container">
-				<a href="https://github.com/' . $userAPI['login'] . '" class="gmpw-head-link">
-					<div class="gmpw-head">
-					  <div class="gmpw-headder">
-							<svg aria-hidden="true" height="18" version="1.1" viewBox="0 0 16 16" width="18"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg>
-							GitHub
-						</div>
-						<div class="gmpw-profile-picture">
-							<img src="' . $userAPI['avatar_url'] . '"  />
-						</div>
-						<div class="gmpw-names">
-							<div class="gmpw-name">
-								' . $userAPI['name'] . '
-							</div>
-							<div class="gmpw-user">
-								@' . $userAPI['login'] . '
-							</div>
-						</div>
-					</a>
-				</div>';
+		// Set the cache name for this instance of the widget
+		$cache = get_transient('wpgpw' . md5(serialize($atts)));
 
-				if ($userAPI['bio'] != '')
-				{
-					$widget .= '
-					<div class="gmpw-bio">
-						<span>Bio: </span>
-						' . $userAPI['bio'] . '
+		if ($cache)
+		{
+				// If the cache exists, return it rather than re-creating it
+				echo $cache;
+		}
+		else
+		{
+			// Get the API results
+			$userAPI = $this->f13_get_github_api('https://api.github.com/users/' . $github_user);
+			$widget = '
+				<div class="gmpw-container">
+					<a href="https://github.com/' . $userAPI['login'] . '" class="gmpw-head-link">
+						<div class="gmpw-head">
+						  <div class="gmpw-headder">
+								<svg aria-hidden="true" height="18" version="1.1" viewBox="0 0 16 16" width="18"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg>
+								GitHub
+							</div>
+							<div class="gmpw-profile-picture">
+								<img src="' . $userAPI['avatar_url'] . '"  />
+							</div>
+							<div class="gmpw-names">
+								<div class="gmpw-name">
+									' . $userAPI['name'] . '
+								</div>
+								<div class="gmpw-user">
+									@' . $userAPI['login'] . '
+								</div>
+							</div>
+						</a>
 					</div>';
-				}
 
-				$widget .= '
-				<div class="gmpw-info">
-					<span class="gmpw-info-user">
-						<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 14 16" width="14"><path d="M4.75 4.95C5.3 5.59 6.09 6 7 6c.91 0 1.7-.41 2.25-1.05A1.993 1.993 0 0 0 13 4c0-1.11-.89-2-2-2-.41 0-.77.13-1.08.33A3.01 3.01 0 0 0 7 0C5.58 0 4.39 1 4.08 2.33 3.77 2.13 3.41 2 3 2c-1.11 0-2 .89-2 2a1.993 1.993 0 0 0 3.75.95zm5.2-1.52c.2-.38.59-.64 1.05-.64.66 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2-.65 0-1.17-.53-1.19-1.17.06-.19.11-.39.14-.59zM7 .98c1.11 0 2.02.91 2.02 2.02 0 1.11-.91 2.02-2.02 2.02-1.11 0-2.02-.91-2.02-2.02C4.98 1.89 5.89.98 7 .98zM3 5.2c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.45 0 .84.27 1.05.64.03.2.08.41.14.59C4.17 4.67 3.66 5.2 3 5.2zM13 6H1c-.55 0-1 .45-1 1v3c0 .55.45 1 1 1v2c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-1h1v3c0 .55.45 1 1 1h2c.55 0 1-.45 1-1v-3h1v1c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-2c.55 0 1-.45 1-1V7c0-.55-.45-1-1-1zM3 13H2v-3H1V7h2v6zm7-2H9V9H8v6H6V9H5v2H4V7h6v4zm3-1h-1v3h-1V7h2v3z"></path></svg>
-						' . $userAPI['login'] . '<br />';
-
-						if ($userAPI['location'] != '')
-						{
-							$widget .= '
-							<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 12 16" width="12"><path d="M6 0C2.69 0 0 2.5 0 5.5 0 10.02 6 16 6 16s6-5.98 6-10.5C12 2.5 9.31 0 6 0zm0 14.55C4.14 12.52 1 8.44 1 5.5 1 3.02 3.25 1 6 1c1.34 0 2.61.48 3.56 1.36.92.86 1.44 1.97 1.44 3.14 0 2.94-3.14 7.02-5 9.05zM8 5.5c0 1.11-.89 2-2 2-1.11 0-2-.89-2-2 0-1.11.89-2 2-2 1.11 0 2 .89 2 2z"></path></svg>
-							' . $userAPI['location'] . '<br />';
-						}
-
-						if ($userAPI['email'] != '')
-						{
-							$widget .= '
-							<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 14 16" width="14"><path d="M0 4v8c0 .55.45 1 1 1h12c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1H1c-.55 0-1 .45-1 1zm13 0L7 9 1 4h12zM1 5.5l4 3-4 3v-6zM2 12l3.5-3L7 10.5 8.5 9l3.5 3H2zm11-.5l-4-3 4-3v6z"></path></svg>
-							<a href="mailto:' . $userAPI['email'] . '">' . $userAPI['email'] . '</a>';
-						}
+					if ($userAPI['bio'] != '')
+					{
+						$widget .= '
+						<div class="gmpw-bio">
+							<span>Bio: </span>
+							' . $userAPI['bio'] . '
+						</div>';
+					}
 
 					$widget .= '
-					</span>
-					<span class="gmpw-info-website">';
+					<div class="gmpw-info">
+						<span class="gmpw-info-user">
+							<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 14 16" width="14"><path d="M4.75 4.95C5.3 5.59 6.09 6 7 6c.91 0 1.7-.41 2.25-1.05A1.993 1.993 0 0 0 13 4c0-1.11-.89-2-2-2-.41 0-.77.13-1.08.33A3.01 3.01 0 0 0 7 0C5.58 0 4.39 1 4.08 2.33 3.77 2.13 3.41 2 3 2c-1.11 0-2 .89-2 2a1.993 1.993 0 0 0 3.75.95zm5.2-1.52c.2-.38.59-.64 1.05-.64.66 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2-.65 0-1.17-.53-1.19-1.17.06-.19.11-.39.14-.59zM7 .98c1.11 0 2.02.91 2.02 2.02 0 1.11-.91 2.02-2.02 2.02-1.11 0-2.02-.91-2.02-2.02C4.98 1.89 5.89.98 7 .98zM3 5.2c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.45 0 .84.27 1.05.64.03.2.08.41.14.59C4.17 4.67 3.66 5.2 3 5.2zM13 6H1c-.55 0-1 .45-1 1v3c0 .55.45 1 1 1v2c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-1h1v3c0 .55.45 1 1 1h2c.55 0 1-.45 1-1v-3h1v1c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-2c.55 0 1-.45 1-1V7c0-.55-.45-1-1-1zM3 13H2v-3H1V7h2v6zm7-2H9V9H8v6H6V9H5v2H4V7h6v4zm3-1h-1v3h-1V7h2v3z"></path></svg>
+							' . $userAPI['login'] . '<br />';
 
-						if ($userAPI['blog'] != '')
-						{
-							$widget .= '
-							<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>
-							<a href="' . $userAPI['blog'] . '">' . $userAPI['blog'] . '</a><br />';
-						}
+							if ($userAPI['location'] != '')
+							{
+								$widget .= '
+								<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 12 16" width="12"><path d="M6 0C2.69 0 0 2.5 0 5.5 0 10.02 6 16 6 16s6-5.98 6-10.5C12 2.5 9.31 0 6 0zm0 14.55C4.14 12.52 1 8.44 1 5.5 1 3.02 3.25 1 6 1c1.34 0 2.61.48 3.56 1.36.92.86 1.44 1.97 1.44 3.14 0 2.94-3.14 7.02-5 9.05zM8 5.5c0 1.11-.89 2-2 2-1.11 0-2-.89-2-2 0-1.11.89-2 2-2 1.11 0 2 .89 2 2z"></path></svg>
+								' . $userAPI['location'] . '<br />';
+							}
+
+							if ($userAPI['email'] != '')
+							{
+								$widget .= '
+								<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 14 16" width="14"><path d="M0 4v8c0 .55.45 1 1 1h12c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1H1c-.55 0-1 .45-1 1zm13 0L7 9 1 4h12zM1 5.5l4 3-4 3v-6zM2 12l3.5-3L7 10.5 8.5 9l3.5 3H2zm11-.5l-4-3 4-3v6z"></path></svg>
+								<a href="mailto:' . $userAPI['email'] . '">' . $userAPI['email'] . '</a>';
+							}
 
 						$widget .= '
-						<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 14 16" width="14"><path d="M8 8h3v2H7c-.55 0-1-.45-1-1V4h2v4zM7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7z"></path></svg>
-						Joined on ' . $this->gitDate($userAPI['created_at']) . '
-					</span>
-				</div>';
-				// Change to if show numbers
-				if (true)
-				{
-					$starredCount = count($this->f13_get_github_api('https://api.github.com/users/' . $github_user . '/starred'));
-					$widget .= '
-					<div class="gmpw-numbers">
-						<a href="#">
-							<span>
-								<span>' . $userAPI['followers'] . '</span><br />
-								Follower
-							</span>
-						</a>
-						<a href="#">
-							<span>
-								<span>' . $starredCount . '</span><br />
-								Starred
-							</span>
-						</a>
-						<a href="#">
-							<span>
-								<span>' . $userAPI['following'] . '</span><br />
-								Following
-							</span>
-						</a>
-					</div>';
-				}
-				$widget .= '
-				<div class="gmpw-repos">
-					<span class="gmpw-repos-public">
-						<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>
-						<a href="https://github.com/' . $userAPI['login'] . '/repositories">' . $userAPI['public_repos'] . ' Public Repositories</a>
 						</span>
-					<span class="gmpw-repos-gists">
-						<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>
-						<a href="https://gists.github.com/' . $userAPI['login'] . '">' . $userAPI['public_gists'] . ' Public Gists</a>
-					</span>
+						<span class="gmpw-info-website">';
+
+							if ($userAPI['blog'] != '')
+							{
+								$widget .= '
+								<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>
+								<a href="' . $userAPI['blog'] . '">' . $userAPI['blog'] . '</a><br />';
+							}
+
+							$widget .= '
+							<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 14 16" width="14"><path d="M8 8h3v2H7c-.55 0-1-.45-1-1V4h2v4zM7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7z"></path></svg>
+							Joined on ' . $this->gitDate($userAPI['created_at']) . '
+						</span>
+					</div>';
+					// Change to if show numbers
+					if (true)
+					{
+						$starredCount = count($this->f13_get_github_api('https://api.github.com/users/' . $github_user . '/starred'));
+						$widget .= '
+						<div class="gmpw-numbers">
+							<a href="#">
+								<span>
+									<span>' . $userAPI['followers'] . '</span><br />
+									Follower
+								</span>
+							</a>
+							<a href="#">
+								<span>
+									<span>' . $starredCount . '</span><br />
+									Starred
+								</span>
+							</a>
+							<a href="#">
+								<span>
+									<span>' . $userAPI['following'] . '</span><br />
+									Following
+								</span>
+							</a>
+						</div>';
+					}
+					$widget .= '
+					<div class="gmpw-repos">
+						<span class="gmpw-repos-public">
+							<svg aria-hidden="true" class="octicon octicon-repo" height="16" version="1.1" viewBox="0 0 12 16" width="12"><path d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"></path></svg>
+							<a href="https://gists.github.com/' . $userAPI['login'] . '">' . $userAPI['public_repos'] . ' Public Repos</a>
+							</span>
+						<span class="gmpw-repos-gists">
+							<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 12 16" width="16"><path d="M7.5 5L10 7.5 7.5 10l-.75-.75L8.5 7.5 6.75 5.75 7.5 5zm-3 0L2 7.5 4.5 10l.75-.75L3.5 7.5l1.75-1.75L4.5 5zM0 13V2c0-.55.45-1 1-1h10c.55 0 1 .45 1 1v11c0 .55-.45 1-1 1H1c-.55 0-1-.45-1-1zm1 0h10V2H1v11z"></path></svg>
+							<a href="https://gists.github.com/' . $userAPI['login'] . '">' . $userAPI['public_gists'] . ' Public Gists</a>
+						</span>
+					</div>
 				</div>
-			</div>
-		';
-		echo $widget;
+			';
+			set_transient('wpgpw' . md5(serialize($atts)), $widget, $github_timeout);
+			echo $widget;
+		}
 	}
 
 	private function f13_get_github_api($url)
