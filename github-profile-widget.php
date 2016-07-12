ddbd8b2452ccea14afb28af0a024924720b3912a
James Valentine <jv@f13dev.com>
2016-07-12 19:48:03 +0100

    Improved styling

--- github-profile-widget.php
+++ github-profile-widget.php
@@ -32,14 +32,18 @@ add_action('widgets_init', create_function('', 'return register_widget("GitHub_M
 // Register the css
 add_action( 'wp_enqueue_scripts', 'gmpw_style');
 
-// Add the stylesheet
+/**
+ * A function to register and enque the stylesheet
+ */
 function gmpw_style()
 {
 	wp_register_style( 'f13-gmpw-style', plugins_url('github-profile-widget.css', __FILE__) );
   wp_enqueue_style( 'f13-gmpw-style' );
 }
 
-// Widget class
+/**
+ * A class to generate a GitHub profile widget
+ */
 class GitHub_Mini_Profile_Widget extends WP_Widget
 {
 	/** Basic Widget Settings */
@@ -50,7 +54,8 @@ class GitHub_Mini_Profile_Widget extends WP_Widget
 	var $fields;
 
 	/**
-	* Construct the widget
+	* Create a new instance of the GitHub widget
+	* by setting the widget setting fields.
 	*/
 	function __construct()
 	{
@@ -60,6 +65,7 @@ class GitHub_Mini_Profile_Widget extends WP_Widget
 		$this->add_field('title', 'Enter title', '', 'text');
 		$this->add_field('github_user', 'GitHub ID', '', 'text');
 		$this->add_field('github_token', 'GitHub API Token', '', 'text');
+		$this->add_field('option_show_figures', 'Show figures', true, 'checkbox');
 		//Init the widget
 		parent::__construct($this->textdomain, __(self::WIDGET_NAME, $this->textdomain), array( 'description' => __(self::WIDGET_DESCRIPTION, $this->textdomain), 'classname' => $this->textdomain));
 	}
@@ -121,17 +127,21 @@ class GitHub_Mini_Profile_Widget extends WP_Widget
 		/* Generate admin form fields */
 		foreach($this->fields as $field_name => $field_data)
 		{
-			if($field_data['type'] === 'text'):
+			if($field_data['type'] === 'text')
+			{
 				?>
 				<p>
 					<label for="<?php echo $this->get_field_id($field_name); ?>"><?php _e($field_data['description'], $this->textdomain ); ?></label>
 					<input class="widefat" id="<?php echo $this->get_field_id($field_name); ?>" name="<?php echo $this->get_field_name($field_name); ?>" type="text" value="<?php echo esc_attr(isset($instance[$field_name]) ? $instance[$field_name] : $field_data['default_value']); ?>" />
 				</p>
 				<?php
+
+			}
+			else
+			{
 				/* Otherwise show an error */
-			else:
 				echo __('Error - Field type not supported', $this->textdomain) . ': ' . $field_data['type'];
-			endif;
+			}
 		}
 	}
 
@@ -161,11 +171,11 @@ class GitHub_Mini_Profile_Widget extends WP_Widget
 				<a href="https://github.com/' . $userAPI['login'] . '" class="gmpw-head-link">
 					<div class="gmpw-head">
 					  <div class="gmpw-headder">
-							<img src="' . $userAPI['avatar_url'] . '" />
+							<svg aria-hidden="true" height="18" version="1.1" viewBox="0 0 16 16" width="18"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg>
 							GitHub
 						</div>
 						<div class="gmpw-profile-picture">
-							<img src="https://avatars1.githubusercontent.com/u/18120373?v=3&s=460"  />
+							<img src="' . $userAPI['avatar_url'] . '"  />
 						</div>
 						<div class="gmpw-names">
 							<div class="gmpw-name">
@@ -176,11 +186,18 @@ class GitHub_Mini_Profile_Widget extends WP_Widget
 							</div>
 						</div>
 					</a>
-				</div>
-				<div class="gmpw-bio">
-					<span>Bio: </span>
-					' . $userAPI['bio'] . '
-				</div>
+				</div>';
+
+				if ($userAPI['bio'] != '')
+				{
+					$widget .= '
+					<div class="gmpw-bio">
+						<span>Bio: </span>
+						' . $userAPI['bio'] . '
+					</div>';
+				}
+
+				$widget .= '
 				<div class="gmpw-info">
 					<span class="gmpw-info-user">
 						<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 14 16" width="14"><path d="M4.75 4.95C5.3 5.59 6.09 6 7 6c.91 0 1.7-.41 2.25-1.05A1.993 1.993 0 0 0 13 4c0-1.11-.89-2-2-2-.41 0-.77.13-1.08.33A3.01 3.01 0 0 0 7 0C5.58 0 4.39 1 4.08 2.33 3.77 2.13 3.41 2 3 2c-1.11 0-2 .89-2 2a1.993 1.993 0 0 0 3.75.95zm5.2-1.52c.2-.38.59-.64 1.05-.64.66 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2-.65 0-1.17-.53-1.19-1.17.06-.19.11-.39.14-.59zM7 .98c1.11 0 2.02.91 2.02 2.02 0 1.11-.91 2.02-2.02 2.02-1.11 0-2.02-.91-2.02-2.02C4.98 1.89 5.89.98 7 .98zM3 5.2c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.45 0 .84.27 1.05.64.03.2.08.41.14.59C4.17 4.67 3.66 5.2 3 5.2zM13 6H1c-.55 0-1 .45-1 1v3c0 .55.45 1 1 1v2c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-1h1v3c0 .55.45 1 1 1h2c.55 0 1-.45 1-1v-3h1v1c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-2c.55 0 1-.45 1-1V7c0-.55-.45-1-1-1zM3 13H2v-3H1V7h2v6zm7-2H9V9H8v6H6V9H5v2H4V7h6v4zm3-1h-1v3h-1V7h2v3z"></path></svg>
@@ -196,7 +213,7 @@ class GitHub_Mini_Profile_Widget extends WP_Widget
 						if ($userAPI['email'] != '')
 						{
 							$widget .= '
-							<svg aria-hidden="true" class="octicon octicon-mail" height="16" version="1.1" viewBox="0 0 14 16" width="14"><path d="M0 4v8c0 .55.45 1 1 1h12c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1H1c-.55 0-1 .45-1 1zm13 0L7 9 1 4h12zM1 5.5l4 3-4 3v-6zM2 12l3.5-3L7 10.5 8.5 9l3.5 3H2zm11-.5l-4-3 4-3v6z"></path></svg>
+							<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 14 16" width="14"><path d="M0 4v8c0 .55.45 1 1 1h12c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1H1c-.55 0-1 .45-1 1zm13 0L7 9 1 4h12zM1 5.5l4 3-4 3v-6zM2 12l3.5-3L7 10.5 8.5 9l3.5 3H2zm11-.5l-4-3 4-3v6z"></path></svg>
 							<a href="mailto:' . $userAPI['email'] . '">' . $userAPI['email'] . '</a>';
 						}
 
@@ -212,8 +229,8 @@ class GitHub_Mini_Profile_Widget extends WP_Widget
 						}
 
 						$widget .= '
-						<svg aria-hidden="true" class="octicon octicon-clock" height="16" version="1.1" viewBox="0 0 14 16" width="14"><path d="M8 8h3v2H7c-.55 0-1-.45-1-1V4h2v4zM7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7z"></path></svg>
-						Joined on Mar 28, 2016
+						<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 14 16" width="14"><path d="M8 8h3v2H7c-.55 0-1-.45-1-1V4h2v4zM7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7z"></path></svg>
+						Joined on ' . $this->gitDate($userAPI['created_at']) . '
 					</span>
 				</div>';
 				// Change to if show numbers
@@ -244,8 +261,14 @@ class GitHub_Mini_Profile_Widget extends WP_Widget
 				}
 				$widget .= '
 				<div class="gmpw-repos">
-					<span class="gmpw-repos-public"><a href="https://github.com/' . $userAPI['login'] . '/repositories">' . $userAPI['public_repos'] . ' Public Repositories</a></span>
-					<span class="gmpw-repos-gists"><a href="https://gists.github.com/' . $userAPI['login'] . '">' . $userAPI['public_gists'] . ' Public Gists</a></span>
+					<span class="gmpw-repos-public">
+						<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>
+						<a href="https://github.com/' . $userAPI['login'] . '/repositories">' . $userAPI['public_repos'] . ' Public Repositories</a>
+						</span>
+					<span class="gmpw-repos-gists">
+						<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>
+						<a href="https://gists.github.com/' . $userAPI['login'] . '">' . $userAPI['public_gists'] . ' Public Gists</a>
+					</span>
 				</div>
 			</div>
 		';
@@ -295,6 +318,81 @@ class GitHub_Mini_Profile_Widget extends WP_Widget
 
 			 // Return the results
 			 return $result;
+	 }
+
+	 private function gitDate($date)
+	 {
+		 $dateArray = explode('-', $date);
+		 // Add the day to the string
+		 $date = substr($dateArray[2], 0, 2) . ' ';
+		 // Add the month to the string
+		 $date .= $this->getMonth($dateArray[1]) . ' ';
+		 // Add the year
+		 $date .= $dateArray[0];
+		 return $date;
+	 }
+
+	 private function getMonth($month)
+	 {
+		 if ($month == 01)
+		 {
+			 return 'Jan';
+		 }
+		 else
+		 if ($month == 02)
+		 {
+			 return 'Feb';
+		 }
+		 else
+		 if ($month == 03)
+		 {
+			 return 'Mar';
+		 }
+		 else
+		 if ($month == 04)
+		 {
+			 return 'Apr';
+		 }
+		 else
+		 if ($month == 05)
+		 {
+			 return 'May';
+		 }
+		 else
+		 if ($month == 06)
+		 {
+			 return 'Jun';
+		 }
+		 else
+		 if ($month == 07)
+		 {
+			 return 'Jul';
+		 }
+		 else
+		 if ($month == 08)
+		 {
+			 return 'Aug';
+		 }
+		 else
+		 if ($month == 09)
+		 {
+			 return 'Sep';
+		 }
+		 else
+		 if ($month == 10)
+		 {
+			 return 'Oct';
+		 }
+		 else
+		 if ($month == 11)
+		 {
+			 return 'Nov';
+		 }
+		 else
+		 if ($month == 12)
+		 {
+			 return 'Dec';
+		 }
 	 }
 
 }
