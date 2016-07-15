233cd8673c0863a39060357fed0dc03de1ee9e75
f13dev <jv@f13dev.com>
2016-07-15 09:19:57 +0100

    Adding correct url link for user repositories

--- github-profile-widget.php
+++ github-profile-widget.php
@@ -279,7 +279,7 @@ class GitHub_Mini_Profile_Widget extends WP_Widget
 					<div class="gmpw-repos">
 						<span class="gmpw-repos-public">
 							<svg aria-hidden="true" class="octicon octicon-repo" height="16" version="1.1" viewBox="0 0 12 16" width="12"><path d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"></path></svg>
-							<a href="https://gists.github.com/' . $userAPI['login'] . '">' . $userAPI['public_repos'] . ' Public Repos</a>
+							<a href="https://github.com/' . $userAPI['login'] . '/repositories">' . $userAPI['public_repos'] . ' Public Repos</a>
 							</span>
 						<span class="gmpw-repos-gists">
 							<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 12 16" width="16"><path d="M7.5 5L10 7.5 7.5 10l-.75-.75L8.5 7.5 6.75 5.75 7.5 5zm-3 0L2 7.5 4.5 10l.75-.75L3.5 7.5l1.75-1.75L4.5 5zM0 13V2c0-.55.45-1 1-1h10c.55 0 1 .45 1 1v11c0 .55-.45 1-1 1H1c-.55 0-1-.45-1-1zm1 0h10V2H1v11z"></path></svg>
